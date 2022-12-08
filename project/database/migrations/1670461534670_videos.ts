a7be13567808569439b57a70d63840fcfdca35b1
Matheus Oliveira <107084202+MatheusOliveira08@users.noreply.github.com>
2022-12-08 00:08:50 -0300

    Criando Banco de Dados SQLite

---
+++ project/database/migrations/1670461534670_videos.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'videos'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('titulo').notNullable()
+      table.string('description').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
