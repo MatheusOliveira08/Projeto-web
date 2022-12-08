16c01bf4c4eee1d0fc362aa70d8d35bb2dec037c
Matheus Soares <matheusmontenegro2011@hotmail.com>
2022-12-08 11:33:06 -0300

    Adicionei Auth model e a table Auth

--- project/database/migrations/1670509566666_auths.ts
+++ project/database/migrations/1670509566666_auths.ts
@@ -1,17 +1,15 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'videos'
+  protected tableName = 'auths'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('titulo').notNullable()
-      table.string('description').notNullable()
-      
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+
+      table.string('email').notNullable()
+      table.string('senha').notNullable()
+
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
