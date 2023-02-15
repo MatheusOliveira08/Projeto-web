74924f28d798617966c857579578afb0147c7139
Matheus Soares <matheusmontenegro2011@hotmail.com>
2023-02-15 17:35:30 -0300

    Ajeitando o histórico

--- project/database/migrations/1675953895313_video_histories.ts
+++ project/database/migrations/1675953895313_video_histories.ts
@@ -8,6 +8,8 @@ export default class extends BaseSchema {
       table.increments('id')
       table.integer('video_id').unsigned().references('id').inTable('videos').onDelete('CASCADE').notNullable()
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').notNullable()
+      table.string('titulo').notNullable()
+      table.string('username').notNullable()
       table.timestamp('created_at', { useTz: true })
     })
   }
